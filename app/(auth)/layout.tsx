c0e1f2ae17d2d6b432a37f39cf7379770480683a
uttam <uttam@192.168.1.4>
2024-01-02 12:32:44 +0545

    reset

--- app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className="grid h-screen place-content-center">{children}</div>;
-}
--- components/avatar.tsx
+++
@@ -1,9 +0,0 @@
-import { Avatar, AvatarImage } from "@/components/ui/avatar";
-
-export default function AvatarEl({ src }: { src: string | undefined }) {
-  return (
-    <Avatar>
-      <AvatarImage src={src} />
-    </Avatar>
-  );
-}
